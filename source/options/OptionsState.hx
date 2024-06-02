5346702d690aab33e3ef003b33fc8a76db3f987f
Bunny <84008574+GhostBnuuy@users.noreply.github.com>
2024-06-02 22:10:51 +0200

    Welp, hello again psych engine

---
+++ source/options/OptionsState.hx
@@ -0,0 +1,137 @@
+package options;
+
+import states.MainMenuState;
+import backend.StageData;
+
+class OptionsState extends MusicBeatState
+{
+	var options:Array<String> = [
+		'Note Colors',
+		'Controls',
+		'Adjust Delay and Combo',
+		'Graphics',
+		'Visuals',
+		'Gameplay'
+		#if TRANSLATIONS_ALLOWED , 'Language' #end
+	];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var menuBG:FlxSprite;
+	public static var onPlayState:Bool = false;
+
+	function openSelectedSubstate(label:String) {
+		switch(label)
+		{
+			case 'Note Colors':
+				openSubState(new options.NotesSubState());
+			case 'Controls':
+				openSubState(new options.ControlsSubState());
+			case 'Graphics':
+				openSubState(new options.GraphicsSettingsSubState());
+			case 'Visuals':
+				openSubState(new options.VisualsSettingsSubState());
+			case 'Gameplay':
+				openSubState(new options.GameplaySettingsSubState());
+			case 'Adjust Delay and Combo':
+				MusicBeatState.switchState(new options.NoteOffsetState());
+			case 'Language':
+				openSubState(new options.LanguageSubState());
+		}
+	}
+
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
+
+	override function create()
+	{
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		bg.color = 0xFFea71fd;
+		bg.updateHitbox();
+
+		bg.screenCenter();
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (num => option in options)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, Language.getPhrase('options_$option', option), true);
+			optionText.screenCenter();
+			optionText.y += (92 * (num - (options.length / 2))) + 45;
+			grpOptions.add(optionText);
+		}
+
+		selectorLeft = new Alphabet(0, 0, '>', true);
+		add(selectorLeft);
+		selectorRight = new Alphabet(0, 0, '<', true);
+		add(selectorRight);
+
+		changeSelection();
+		ClientPrefs.saveSettings();
+
+		super.create();
+	}
+
+	override function closeSubState()
+	{
+		super.closeSubState();
+		ClientPrefs.saveSettings();
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (controls.UI_UP_P)
+			changeSelection(-1);
+		if (controls.UI_DOWN_P)
+			changeSelection(1);
+
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			if(onPlayState)
+			{
+				StageData.loadDirectory(PlayState.SONG);
+				LoadingState.loadAndSwitchState(new PlayState());
+				FlxG.sound.music.volume = 0;
+			}
+			else MusicBeatState.switchState(new MainMenuState());
+		}
+		else if (controls.ACCEPT) openSelectedSubstate(options[curSelected]);
+	}
+	
+	function changeSelection(change:Int = 0)
+	{
+		curSelected = FlxMath.wrap(curSelected + change, 0, options.length - 1);
+
+		for (num => item in grpOptions.members)
+		{
+			item.targetY = num - curSelected;
+			item.alpha = 0.6;
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+
+	override function destroy()
+	{
+		ClientPrefs.loadPrefs();
+		super.destroy();
+	}
+}
---
+++ source/states/LoadingState.hx
@@ -0,0 +1,586 @@
+package states;
+
+import haxe.Json;
+import lime.utils.Assets;
+import openfl.display.BitmapData;
+import openfl.utils.AssetType;
+import openfl.utils.Assets as OpenFlAssets;
+import flixel.graphics.FlxGraphic;
+import flixel.FlxState;
+
+import backend.Song;
+import backend.StageData;
+import objects.Character;
+
+import sys.thread.Thread;
+import sys.thread.Mutex;
+
+import objects.Note;
+import objects.NoteSplash;
+
+class LoadingState extends MusicBeatState
+{
+	public static var loaded:Int = 0;
+	public static var loadMax:Int = 0;
+
+	static var originalBitmapKeys:Map<String, String> = [];
+	static var requestedBitmaps:Map<String, BitmapData> = [];
+	static var mutex:Mutex = new Mutex();
+
+	function new(target:FlxState, stopMusic:Bool)
+	{
+		this.target = target;
+		this.stopMusic = stopMusic;
+		
+		super();
+	}
+
+	inline static public function loadAndSwitchState(target:FlxState, stopMusic = false, intrusive:Bool = true)
+		MusicBeatState.switchState(getNextState(target, stopMusic, intrusive));
+	
+	var target:FlxState = null;
+	var stopMusic:Bool = false;
+	var dontUpdate:Bool = false;
+
+	var bar:FlxSprite;
+	var barWidth:Int = 0;
+	var intendedPercent:Float = 0;
+	var curPercent:Float = 0;
+	var canChangeState:Bool = true;
+
+	#if PSYCH_WATERMARKS
+	var logo:FlxSprite;
+	var pessy:FlxSprite;
+	var loadingText:FlxText;
+
+	var timePassed:Float;
+	var shakeFl:Float;
+	var shakeMult:Float = 0;
+	
+	var isSpinning:Bool = false;
+	var spawnedPessy:Bool = false;
+	var pressedTimes:Int = 0;
+	#else
+	var funkay:FlxSprite;
+	#end
+
+	override function create()
+	{
+		#if !SHOW_LOADING_SCREEN
+		while(true)
+		#end
+		{
+			if (checkLoaded())
+			{
+				dontUpdate = true;
+				super.create();
+				onLoad();
+				return;
+			}
+			#if !SHOW_LOADING_SCREEN
+			Sys.sleep(0.01);
+			#end
+		}
+
+		#if PSYCH_WATERMARKS // PSYCH LOADING SCREEN
+		var bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.setGraphicSize(Std.int(FlxG.width));
+		bg.color = 0xFFD16FFF;
+		bg.updateHitbox();
+		add(bg);
+	
+		loadingText = new FlxText(520, 600, 400, Language.getPhrase('now_loading', 'Now Loading', ['...']), 32);
+		loadingText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, LEFT, OUTLINE_FAST, FlxColor.BLACK);
+		loadingText.borderSize = 2;
+		add(loadingText);
+	
+		logo = new FlxSprite(0, 0).loadGraphic(Paths.image('loading_screen/icon'));
+		logo.scale.set(0.75, 0.75);
+		logo.updateHitbox();
+		logo.antialiasing = ClientPrefs.data.antialiasing;
+		logo.screenCenter();
+		logo.x -= 50;
+		logo.y -= 40;
+		add(logo);
+
+		#else // BASE GAME LOADING SCREEN
+		var bg = new FlxSprite().makeGraphic(1, 1, 0xFFCAFF4D);
+		bg.scale.set(FlxG.width, FlxG.height);
+		bg.updateHitbox();
+		bg.screenCenter();
+		add(bg);
+
+		funkay = new FlxSprite(0, 0).loadGraphic(Paths.image('funkay'));
+		funkay.antialiasing = ClientPrefs.data.antialiasing;
+		funkay.setGraphicSize(0, FlxG.height);
+		funkay.updateHitbox();
+		add(funkay);
+		#end
+
+		var bg:FlxSprite = new FlxSprite(0, 660).makeGraphic(1, 1, FlxColor.BLACK);
+		bg.scale.set(FlxG.width - 300, 25);
+		bg.updateHitbox();
+		bg.screenCenter(X);
+		add(bg);
+
+		bar = new FlxSprite(bg.x + 5, bg.y + 5).makeGraphic(1, 1, FlxColor.WHITE);
+		bar.scale.set(0, 15);
+		bar.updateHitbox();
+		add(bar);
+		barWidth = Std.int(bg.width - 10);
+
+		persistentUpdate = true;
+		super.create();
+	}
+
+	var transitioning:Bool = false;
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		if (dontUpdate) return;
+
+		if (!transitioning)
+		{
+			if (canChangeState && !finishedLoading && checkLoaded())
+			{
+				transitioning = true;
+				onLoad();
+				return;
+			}
+			intendedPercent = loaded / loadMax;
+		}
+
+		if (curPercent != intendedPercent)
+		{
+			if (Math.abs(curPercent - intendedPercent) < 0.001) curPercent = intendedPercent;
+			else curPercent = FlxMath.lerp(intendedPercent, curPercent, Math.exp(-elapsed * 15));
+
+			bar.scale.x = barWidth * curPercent;
+			bar.updateHitbox();
+		}
+
+		#if PSYCH_WATERMARKS // PSYCH LOADING SCREEN
+		timePassed += elapsed;
+		shakeFl += elapsed * 3000;
+		var dots:String = '';
+		switch(Math.floor(timePassed % 1 * 3))
+		{
+			case 0:
+				dots = '.';
+			case 1:
+				dots = '..';
+			case 2:
+				dots = '...';
+		}
+		loadingText.text = Language.getPhrase('now_loading', 'Now Loading{1}', [dots]);
+
+		if(!spawnedPessy)
+		{
+			if(!transitioning && controls.ACCEPT)
+			{
+				shakeMult = 1;
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				pressedTimes++;
+			}
+			shakeMult = Math.max(0, shakeMult - elapsed * 5);
+			logo.offset.x = Math.sin(shakeFl * Math.PI / 180) * shakeMult * 100;
+
+			if(pressedTimes >= 5)
+			{
+				FlxG.camera.fade(0xAAFFFFFF, 0.5, true);
+				logo.visible = false;
+				spawnedPessy = true;
+				canChangeState = false;
+				FlxG.sound.play(Paths.sound('secret'));
+
+				pessy = new FlxSprite(700, 140);
+				new FlxTimer().start(0.01, function(tmr:FlxTimer) {
+					pessy.frames = Paths.getSparrowAtlas('loading_screen/pessy');
+					pessy.antialiasing = ClientPrefs.data.antialiasing;
+					pessy.flipX = (logo.offset.x > 0);
+					pessy.x = FlxG.width + 200;
+					pessy.velocity.x = -1100;
+					if(pessy.flipX)
+					{
+						pessy.x = -pessy.width - 200;
+						pessy.velocity.x = 1100;
+					}
+		
+					pessy.animation.addByPrefix('run', 'run', 24, true);
+					pessy.animation.addByPrefix('spin', 'spin', 24, true);
+					pessy.animation.play('run', true);
+					
+					insert(members.indexOf(loadingText), pessy);
+					new FlxTimer().start(5, function(tmr:FlxTimer) canChangeState = true);
+				});
+			}
+		}
+		else if(!isSpinning && (pessy.flipX && pessy.x > FlxG.width) || (!pessy.flipX && pessy.x < -pessy.width))
+		{
+			isSpinning = true;
+			pessy.animation.play('spin', true);
+			pessy.flipX = false;
+			pessy.x = 500;
+			pessy.y = FlxG.height + 500;
+			pessy.velocity.x = 0;
+			FlxTween.tween(pessy, {y: 10}, 0.65, {ease: FlxEase.quadOut});
+		}
+		#end
+	}
+	
+	var finishedLoading:Bool = false;
+	function onLoad()
+	{
+		if (stopMusic && FlxG.sound.music != null)
+			FlxG.sound.music.stop();
+
+		FlxG.camera.visible = false;
+		FlxTransitionableState.skipNextTransIn = true;
+		MusicBeatState.switchState(target);
+		transitioning = true;
+		finishedLoading = true;
+	}
+
+	public static function checkLoaded():Bool
+	{
+		for (key => bitmap in requestedBitmaps)
+		{
+			if (bitmap != null && Paths.cacheBitmap(originalBitmapKeys.get(key), bitmap) != null) trace('finished preloading image $key');
+			else trace('failed to cache image $key');
+		}
+		requestedBitmaps.clear();
+		originalBitmapKeys.clear();
+		return (loaded == loadMax && initialThreadCompleted);
+	}
+
+	public static function loadNextDirectory()
+	{
+		var directory:String = 'shared';
+		var weekDir:String = StageData.forceNextDirectory;
+		StageData.forceNextDirectory = null;
+
+		if (weekDir != null && weekDir.length > 0 && weekDir != '') directory = weekDir;
+
+		Paths.setCurrentLevel(directory);
+		trace('Setting asset folder to ' + directory);
+	}
+
+	static function getNextState(target:FlxState, stopMusic = false, intrusive:Bool = true):FlxState
+	{
+		loadNextDirectory();
+		if(intrusive)
+			return new LoadingState(target, stopMusic);
+
+		if (stopMusic && FlxG.sound.music != null)
+			FlxG.sound.music.stop();
+		
+		while(true)
+		{
+			if(!checkLoaded())
+			{
+				Sys.sleep(0.01);
+			}
+			else break;
+		}
+		return target;
+	}
+
+	static var imagesToPrepare:Array<String> = [];
+	static var soundsToPrepare:Array<String> = [];
+	static var musicToPrepare:Array<String> = [];
+	static var songsToPrepare:Array<String> = [];
+	public static function prepare(images:Array<String> = null, sounds:Array<String> = null, music:Array<String> = null)
+	{
+		if (images != null) imagesToPrepare = imagesToPrepare.concat(images);
+		if (sounds != null) soundsToPrepare = soundsToPrepare.concat(sounds);
+		if (music != null) musicToPrepare = musicToPrepare.concat(music);
+	}
+
+	static var initialThreadCompleted:Bool = true;
+	static var dontPreloadDefaultVoices:Bool = false;
+	public static function prepareToSong()
+	{
+		imagesToPrepare = [];
+		soundsToPrepare = [];
+		musicToPrepare = [];
+		songsToPrepare = [];
+
+		initialThreadCompleted = false;
+		var threadsCompleted:Int = 0;
+		var threadsMax:Int = 2;
+		function completedThread()
+		{
+			threadsCompleted++;
+			if(threadsCompleted == threadsMax)
+			{
+				clearInvalids();
+				startThreads();
+				initialThreadCompleted = true;
+			}
+		}
+
+		var song:SwagSong = PlayState.SONG;
+		var folder:String = Paths.formatToSongPath(song.song);
+		Thread.create(() -> {
+			// LOAD NOTE IMAGE
+			var noteSkin:String = Note.defaultNoteSkin;
+			if(PlayState.SONG.arrowSkin != null && PlayState.SONG.arrowSkin.length > 1) noteSkin = PlayState.SONG.arrowSkin;
+	
+			var customSkin:String = noteSkin + Note.getNoteSkinPostfix();
+			if(Paths.fileExists('images/$customSkin.png', IMAGE)) noteSkin = customSkin;
+			imagesToPrepare.push(noteSkin);
+			//
+
+			// LOAD NOTE SPLASH IMAGE
+			var noteSplash:String = NoteSplash.defaultNoteSplash;
+			if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) noteSplash = PlayState.SONG.splashSkin;
+			else noteSplash += NoteSplash.getSplashSkinPostfix();
+			imagesToPrepare.push(noteSplash);
+
+			try
+			{
+				var path:String = Paths.json('$folder/preload');
+				var json:Dynamic = null;
+
+				#if MODS_ALLOWED
+				var moddyFile:String = Paths.modsJson('$folder/preload');
+				if (FileSystem.exists(moddyFile)) json = Json.parse(File.getContent(moddyFile));
+				else json = Json.parse(File.getContent(path));
+				#else
+				json = Json.parse(Assets.getText(path));
+				#end
+
+				if(json != null)
+				{
+					var imgs:Array<String> = [];
+					var snds:Array<String> = [];
+					var mscs:Array<String> = [];
+					for (asset in Reflect.fields(json))
+					{
+						var filters:Int = Reflect.field(json, asset);
+						var asset:String = asset.trim();
+
+						if(filters < 0 || StageData.validateVisibility(filters))
+						{
+							if(asset.startsWith('images/'))
+								imgs.push(asset.substr('images/'.length));
+							else if(asset.startsWith('sounds/'))
+								snds.push(asset.substr('sounds/'.length));
+							else if(asset.startsWith('music/'))
+								mscs.push(asset.substr('music/'.length));
+						}
+					}
+					prepare(imgs, snds, mscs);
+				}
+			}
+			catch(e:Dynamic) {}
+			completedThread();
+		});
+
+		Thread.create(() -> {
+			if (song.stage == null || song.stage.length < 1)
+				song.stage = StageData.vanillaSongStage(folder);
+
+			var stageData:StageFile = StageData.getStageFile(song.stage);
+			if (stageData != null && stageData.preload != null)
+			{
+				var imgs:Array<String> = [];
+				var snds:Array<String> = [];
+				var mscs:Array<String> = [];
+				for (asset in Reflect.fields(stageData.preload))
+				{
+					var filters:Int = Reflect.field(stageData.preload, asset);
+					var asset:String = asset.trim();
+
+					if(filters < 0 || StageData.validateVisibility(filters))
+					{
+						if(asset.startsWith('images/'))
+							imgs.push(asset.substr('images/'.length));
+						else if(asset.startsWith('sounds/'))
+							snds.push(asset.substr('sounds/'.length));
+						else if(asset.startsWith('music/'))
+							mscs.push(asset.substr('music/'.length));
+					}
+				}
+				prepare(imgs, snds, mscs);
+			}
+
+			songsToPrepare.push('$folder/Inst');
+
+			var player1:String = song.player1;
+			var player2:String = song.player2;
+			var gfVersion:String = song.gfVersion;
+			var prefixVocals:String = song.needsVoices ? '$folder/Voices' : null;
+			if (gfVersion == null) gfVersion = 'gf';
+
+			dontPreloadDefaultVoices = false;
+			preloadCharacter(player1, prefixVocals);
+			if (!dontPreloadDefaultVoices && prefixVocals != null)
+			{
+				if(Paths.fileExists('$prefixVocals-Player.${Paths.SOUND_EXT}', SOUND, false, 'songs') && Paths.fileExists('$prefixVocals-Opponent.${Paths.SOUND_EXT}', SOUND, false, 'songs'))
+				{
+					songsToPrepare.push('$prefixVocals-Player');
+					songsToPrepare.push('$prefixVocals-Opponent');
+				}
+				else if(Paths.fileExists('$prefixVocals.${Paths.SOUND_EXT}', SOUND, false, 'songs'))
+					songsToPrepare.push(prefixVocals);
+			}
+
+			if (player2 != player1)
+			{
+				threadsMax++;
+				Thread.create(() -> {
+					preloadCharacter(player2, prefixVocals);
+					completedThread();
+				});
+			}
+			if (!stageData.hide_girlfriend && gfVersion != player2 && gfVersion != player1)
+			{
+				threadsMax++;
+				Thread.create(() -> {
+					preloadCharacter(gfVersion);
+					completedThread();
+				});
+			}
+			completedThread();
+		});
+	}
+
+	public static function clearInvalids()
+	{
+		clearInvalidFrom(imagesToPrepare, 'images', '.png', IMAGE);
+		clearInvalidFrom(soundsToPrepare, 'sounds', '.${Paths.SOUND_EXT}', SOUND);
+		clearInvalidFrom(musicToPrepare, 'music',' .${Paths.SOUND_EXT}', SOUND);
+		clearInvalidFrom(songsToPrepare, 'songs', '.${Paths.SOUND_EXT}', SOUND, 'songs');
+
+		for (arr in [imagesToPrepare, soundsToPrepare, musicToPrepare, songsToPrepare])
+			while (arr.contains(null))
+				arr.remove(null);
+	}
+
+	static function clearInvalidFrom(arr:Array<String>, prefix:String, ext:String, type:AssetType, ?parentFolder:String = null)
+	{
+		for (i in 0...arr.length)
+		{
+			var folder:String = arr[i];
+			if(folder.trim().endsWith('/'))
+			{
+				for (subfolder in Mods.directoriesWithFile(Paths.getSharedPath(), '$prefix/$folder'))
+					for (file in FileSystem.readDirectory(subfolder))
+						if(file.endsWith(ext))
+							arr.push(folder + file.substr(0, file.length - ext.length));
+
+				//trace('Folder detected! ' + folder);
+			}
+		}
+
+		var i:Int = 0;
+		while(i < arr.length)
+		{
+
+			var member:String = arr[i];
+			var myKey = '$prefix/$member$ext';
+			if(parentFolder == 'songs') myKey = '$member$ext';
+
+			//trace('attempting on $prefix: $myKey');
+			var doTrace:Bool = false;
+			if(member.endsWith('/') || (!Paths.fileExists(myKey, type, false, parentFolder) && (doTrace = true)))
+			{
+				arr.remove(member);
+				if(doTrace) trace('Removed invalid $prefix: $member');
+			}
+			else i++;
+		}
+	}
+
+	public static function startThreads()
+	{
+		loadMax = imagesToPrepare.length + soundsToPrepare.length + musicToPrepare.length + songsToPrepare.length;
+		loaded = 0;
+
+		//then start threads
+		for (sound in soundsToPrepare) initThread(() -> Paths.sound(sound), 'sound $sound');
+		for (music in musicToPrepare) initThread(() -> Paths.music(music), 'music $music');
+		for (song in songsToPrepare) initThread(() -> Paths.returnSound(song, 'songs', true, false), 'song $song');
+
+		// for images, they get to have their own thread
+		for (image in imagesToPrepare)
+			Thread.create(() -> {
+				mutex.acquire();
+				try {
+					var requestKey:String = 'images/$image';
+					#if TRANSLATIONS_ALLOWED requestKey = Language.getFileTranslation(requestKey); #end
+					if(requestKey.lastIndexOf('.') < 0) requestKey += '.png';
+
+					var bitmap:BitmapData;
+					var file:String = Paths.getPath(requestKey, IMAGE);
+					if (Paths.currentTrackedAssets.exists(file)) {
+						mutex.release();
+						loaded++;
+						return;
+					}
+					else if (!OpenFlAssets.exists(file, IMAGE))
+					{
+						trace('no such image $image exists');
+						mutex.release();
+						loaded++;
+						return;
+					}
+					else bitmap = OpenFlAssets.getBitmapData(file);
+					mutex.release();
+
+					if (bitmap != null)
+					{
+						requestedBitmaps.set(file, bitmap);
+						originalBitmapKeys.set(file, requestKey);
+					}
+					else trace('oh no the image is null NOOOO ($image)');
+				}
+				catch(e:Dynamic) {
+					mutex.release();
+					trace('ERROR! fail on preloading image $image');
+				}
+				loaded++;
+			});
+	}
+
+	static function initThread(func:Void->Dynamic, traceData:String)
+	{
+		Thread.create(() -> {
+			mutex.acquire();
+			try {
+				var ret:Dynamic = func();
+				mutex.release();
+
+				if (ret != null) trace('finished preloading $traceData');
+				else trace('ERROR! fail on preloading $traceData');
+			}
+			catch(e:Dynamic) {
+				mutex.release();
+				trace('ERROR! fail on preloading $traceData');
+			}
+			loaded++;
+		});
+	}
+
+	inline private static function preloadCharacter(char:String, ?prefixVocals:String)
+	{
+		try
+		{
+			var path:String = Paths.getPath('characters/$char.json', TEXT);
+			#if MODS_ALLOWED
+			var character:Dynamic = Json.parse(File.getContent(path));
+			#else
+			var character:Dynamic = Json.parse(Assets.getText(path));
+			#end
+			
+			imagesToPrepare.push(character.image);
+			if (prefixVocals != null && character.vocals_file != null)
+			{
+				songsToPrepare.push(prefixVocals + "-" + character.vocals_file);
+				if(char == PlayState.SONG.player1) dontPreloadDefaultVoices = true;
+			}
+		}
+		catch(e:Dynamic) {}
+	}
+}
